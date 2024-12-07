3c411e1c3b209cb1d6ed6e79d9493a7c2109f6fa
ACCS Instructor <189073317+accs-nm-instructor@users.noreply.github.com>
2024-12-07 13:57:09 -0700

    Update week 17

--- week17/main.c
+++ week17/main.c
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    int first = atoi(argv[1]);
-    int second = atoi(argv[2]);
+    int first = atoi(argv[1]);  // <-- this reads the first argument to the command
+    int second = atoi(argv[2]); // <-- this reads the second argument to the command
 
     if (first % 2 == 1)
     {
-        second = -second;
+        second = 0;
     }
     int number = first + second;
 
